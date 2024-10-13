8674892d54c7ca1dd4fe2f8e13c3a8f6804dd701
Acatrinei-Bogdan-Stefan <bogdan.z9.acatrinei@gmail.com>
2024-10-13 09:47:10 +0300

    Version 0.06

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,9 +9,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 
 export class UserComponent {
-  // @Input ({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
   @Input({required: true}) user!: {
     id: string;
     avatar: string;
