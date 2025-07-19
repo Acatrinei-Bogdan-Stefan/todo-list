db553f1b78e543ec86dd11788ebec47852da938f
Acatrinei-Bogdan-Stefan <bogdan.z9.acatrinei@gmail.com>
2025-07-19 14:59:08 +0300

    Version 0.13

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -23,6 +23,13 @@ private tasks = [{
   dueDate: '2024-06-15',
 }];
 
+constructor(){
+  const tasks = localStorage.getItem('tasks');
+  if(tasks){
+    this.tasks = JSON.parse(tasks);
+  }
+}
+
 getUserTasks(userId: string){
     return this.tasks.filter((task) => task.userId === userId);
 }
@@ -35,10 +42,16 @@ addTask(taskData: NewTaskData, userId: string) {
     summary: taskData.summary,
     dueDate: taskData.date
   });
+  this.saveTasks();
 }
 
 removeTask(id: string){
   this.tasks = this.tasks.filter((task) => task.id != id);
+  this.saveTasks();
+}
+
+private saveTasks(){
+  localStorage.setItem('tasks', JSON.stringify(this.tasks));
 }
 
 }
